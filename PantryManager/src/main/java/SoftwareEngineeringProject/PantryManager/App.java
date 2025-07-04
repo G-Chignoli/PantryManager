a006d0d28668f252818203b15d620df8c331fedb
Gabriele Chignoli <g.chignoli1@studenti.unibg.it>
2025-07-04 21:32:42 +0200

    Progetto maven

---
+++ PantryManager/src/main/java/SoftwareEngineeringProject/PantryManager/App.java
@@ -0,0 +1,13 @@
+package SoftwareEngineeringProject.PantryManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
