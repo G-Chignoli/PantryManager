07c23fdb668ce8afa089493918f485d929b7e07d
Gabriele Chignoli <g.chignoli1@studenti.unibg.it>
2025-07-08 17:00:13 +0200

    log4j
    log4j installato data una configurazione base

---
+++ pantrymanager/src/main/java/controller/Product.java
@@ -0,0 +1,24 @@
+package controller;
+import java.util.Date; 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Product {
+	/*
+	private String name = "";
+	private float calories = 0;
+	private Date expirationDate = new Date();  
+	private static Logger logger = LogManager.getLogger(Product.class);
+	
+	public static void main(String[] args)
+	{
+		logger.error("CIAOOOOOONE");
+		logger.trace("We've just greeted the user!");
+		logger.debug("faojbfabwofai!");
+		logger.info("We've just greeted the user!");
+		logger.warn("We've just greeted the user!");
+		logger.error("We've just greeted the user!");
+		logger.fatal("We've just greeted the user!");
+	}
+	*/
+}
