b545ff74bbd48453543518e59073506a4d2ceb03
Gabriele Chignoli <g.chignoli1@studenti.unibg.it>
2025-07-11 15:31:54 +0200

    Classe Product generata
    Aggiunte anche dipendenze per database H2 e framework Hibernate nel POM

--- pantrymanager/src/main/java/controller/Product.java
+++ pantrymanager/src/main/java/controller/Product.java
@@ -1,24 +1,66 @@
 package controller;
 import java.util.Date; 
+import jakarta.persistence.*;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
-public class Product {
-	/*
-	private String name = "";
-	private float calories = 0;
-	private Date expirationDate = new Date();  
-	private static Logger logger = LogManager.getLogger(Product.class);
 	
+public class Product {
+	private String name = "";
+	private float weight = 0;
+	private short qty = 0;
+	private Date expirationDate = new Date();  
+	private float calories = 0;
+	
+	private static Logger logger = LogManager.getLogger(Product.class);
+
 	public static void main(String[] args)
 	{
-		logger.error("CIAOOOOOONE");
-		logger.trace("We've just greeted the user!");
-		logger.debug("faojbfabwofai!");
-		logger.info("We've just greeted the user!");
-		logger.warn("We've just greeted the user!");
-		logger.error("We've just greeted the user!");
-		logger.fatal("We've just greeted the user!");
+		
 	}
-	*/
+	
+	public Product(String name, float weight, short qty, float calories, Date expirationDate) {
+		this.name = name;
+		this.weight = weight;
+		this.qty = qty;
+		this.calories = calories;
+		this.expirationDate = expirationDate; 
+	}
+	
+	public String getName() {
+		return name; 
+	}
+	
+	public float getWeight() {
+		return weight; 
+	}
+	
+	public short getQty() {
+		return qty; 
+	}
+	
+	public float getCalories() {
+		return calories; 
+	}
+	
+	public Date getExpirationDate() {
+		return expirationDate; 
+	}
+	
+	public void setName(String name) {
+		this.name = name; 
+	}
+	
+	public void setWeight(float weight) {
+		this.weight= weight ; 
+	}
+	public void setQty(short qty) {
+		this.qty = qty; 
+	}
+	
+	public void setCalories(float calories) {
+		this.calories = calories; 
+	}
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate; 
+	}
 }
