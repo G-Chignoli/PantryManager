51dda0a5c63c503ceec0dd760b1173fba99f9d8b
GC-Developer 2 <74247720+gabrielechignoli@users.noreply.github.com>
2025-08-13 16:44:56 +0200

    Aggiunta bozza inserimento prodotti
    Non sono tutti i campi necessari. Verrà prossimamente introdotta l'interazione con il db.

--- pantrymanager/src/main/java/view/MainWindow.java
+++ pantrymanager/src/main/java/view/MainWindow.java
@@ -9,16 +9,38 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-
+import javax.swing.JFormattedTextField;
+import javax.swing.JFormattedTextField.AbstractFormatter;
 import javax.swing.Action;
 import net.miginfocom.swing.MigLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
+
+import java.awt.Graphics;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.swing.SwingUtilities;
+import javax.swing.text.DateFormatter;
+
+import org.jdatepicker.JDatePicker;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+import com.toedter.calendar.JDateChooser;
 
 public class MainWindow {
 
@@ -44,20 +66,51 @@ public class MainWindow {
 		
 		JPanel profile_pl = new JPanel();
 		main_frame.getContentPane().add(profile_pl);
-		profile_pl.setLayout(new MigLayout("", "[][][][180.00px,grow][][]", "[][][][][][][][][][][grow]"));
+		profile_pl.setLayout(new MigLayout("", "[grow][grow][grow]", "[grow][grow]"));
 		
 		JLabel profile_title = new JLabel("PROFILI");
-		profile_pl.add(profile_title, "cell 3 0,alignx center,aligny top");
+		profile_pl.add(profile_title, "cell 1 0,alignx center,aligny top");
 		
-		JPanel product_add = new JPanel();
-		profile_pl.add(product_add, "cell 1 9 5 2,grow");
+		// Product Form to add 
 		
-		JLabel lblNewLabel = new JLabel("Aggiungi Prodotto");
-		product_add.add(lblNewLabel);
+		JPanel form_pl = new JPanel();
+		form_pl.setBackground(new Color(200, 200, 255));
+		form_pl.setLayout(new MigLayout("", "[210px][][210px]", "[grow][grow][grow][grow][grow]"));
+		
+		profile_pl.add(form_pl, "cell 1 1, grow, alignx center, aligny center");
+		
+		JLabel form_title = new JLabel("AGGIUNGI PRODOTTO"); 
+		form_pl.add(form_title, "cell 1 0, alignx center, aligny top");
+		
+		JLabel form_name = new JLabel("Nome"); 
+		form_pl.add(form_name, "cell  0 1, alignx right, aligny center");
+		
+		JTextField form_name_tf = new JTextField(15);
+		form_name_tf.setToolTipText("Inserisci il nome del prodotto che vuoi salvare.");
+		form_pl.add(form_name_tf, "cell 1 1, alignx left, aligny center");
+		
+		JLabel form_qty = new JLabel("Quantità"); 
+		form_pl.add(form_qty, "cell  0 2, alignx right, aligny center");
+
+		JTextField form_qty_tf = new JTextField(15);
+		form_name_tf.setToolTipText("Inserisci quante unità vuoi salvare.");
+		form_pl.add(form_qty_tf, "cell 1 2, alignx left, aligny center");
+				
+		JLabel form_exp_date = new JLabel("Data di Scadenza"); 
+		form_pl.add(form_exp_date, "cell  0 3, alignx right, aligny center");
+		
+	    /*
+		JTextField form_exp_date_tf = new JTextField(20);
+		form_exp_date_tf.setToolTipText("Inserisci data");
+		*/
+		
+		JDateChooser chooser = new JDateChooser(new Date(), "dd-mm-yyyy"); 
+		form_pl.add(chooser, "cell 1 3");
+
 		
 		JPanel product_pl = new JPanel();
 		main_frame.getContentPane().add(product_pl);
-		product_pl.setLayout(new MigLayout("", "[100px:n:100px,grow][100px:n:100px,grow][195px:n:195px,grow][100px:n:100px,grow][100px:n:100px,grow]", "[grow][][grow][grow][grow][]"));
+		product_pl.setLayout(new MigLayout("", "[100px:n:100px,grow][100px:n:100px,grow][195px:n:195px,grow][100px:n:100px,grow][100px:n:100px,grow]", "[grow][][grow][grow][grow]"));
 		
 		JLabel product_title = new JLabel("PRODOTTI");
 		product_title.setBackground(new Color(128, 128, 255));
