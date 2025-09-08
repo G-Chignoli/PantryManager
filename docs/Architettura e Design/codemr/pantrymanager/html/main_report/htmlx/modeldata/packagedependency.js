f2497d0474ecf4c1d80af1b9d168e602d20a5fb8
Gabriele Chignoli <g.chignoli1@studenti.unibg.it>
2025-09-08 17:31:40 +0200

    Analisi Statica CodeMR

---
+++ docs/Architettura e Design/codemr/pantrymanager/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,14 @@
+var matrix = [[0,8,0,6],[1,0,0,2],[0,0,0,0],[0,1,0,0]]
+var packages = [{
+"name": " controller", "color": " #3182bd"
+}
+,{
+"name": " view", "color": " #6baed6"
+}
+,{
+"name": " database", "color": " #9ecae1"
+}
+,{
+"name": " model", "color": " #c6dbef"
+}
+];
